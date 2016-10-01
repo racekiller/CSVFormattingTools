017d22fb3ff1d1493965ccc2786f283e80498e20
racekiller <vivasjimmy@gmail.com>
2016-09-30 18:06:37 -0700

    Added String conversion to Number for all columns

--- XOMCSVFormattingTool_version2.py
+++ XOMCSVFormattingTool_version2.py
@@ -9,8 +9,10 @@ NanPHDTagList = []
 #             'XOM BayTown RHC MEA Tower Foaming/Sensor Data'
 
 # Windows Path
-CSVPath1 = 'C:/Users/jvivas/Dropbox/Mtell Customer Projects/XOM BayTown RHC MEA Tower Foaming/Sensor Data/' \
-           'To be Processed'
+#CSVPath1 = 'C:/Users/jvivas/Dropbox/Mtell Customer Projects/XOM BayTown RHC MEA Tower Foaming/Sensor Data/' \
+#           'To be Processed'
+           
+CSVPath1 = 'C:/Mtell/Projects/XOM Baytown POC/Sesor Data/Testing'
 
 # CSVPath2 = '/Users/jvivas/Dropbox/Mtell Customer Projects/XOM BMT Phase 2 Live Monitoring/Sensor Data/System 1 Data'
 
@@ -26,6 +28,7 @@ for i in range(m):
 
 n = len(CSVFileList)
 # In[4]:
+i = 0
 for i in range(n):
     csvfile = CSVPath1 + "/" + CSVFileList[i]
     print("Loading Segment: %s" % CSVFileList[i])
@@ -33,6 +36,16 @@ for i in range(n):
     # In[43]:
     df = pd.read_csv(csvfile, index_col=False, sep=',', low_memory=False)
 
+    # In[100]:
+    # Here we add [UNIT] to the description
+    x = 2   
+    for x in range(len(df.columns)):
+        if (df.iloc[1][x] != ' ') and (df.iloc[1][x] != '-') and (df.iloc[1][x] != 'NONE'):
+            df.iloc[0][x] = df.iloc[0][x] + ' [' + df.iloc[1][x] + ']'
+    
+    # Here we remove the row #1 which is the UNIT row    
+    df = df.drop(df.index[[1]])
+                
     # In[44]:
     # Concatenate DATE and TIME Column
     df['DATETIME2'] = df['DATE'] + ' ' + df['TIME']
@@ -41,8 +54,9 @@ for i in range(n):
     df['DATETIME2'].replace({'-': ''}, inplace=True, regex=True)
     # In[51]:
     # Change new datetime column to datetime format
-    df['DATETIME2'] = pd.to_datetime(df['DATETIME2'], format="%d%b%y %H:%M:%S")
-
+    # df['DATETIME2'] = pd.to_datetime(df['DATETIME2'], format="%d%b%y %H:%M:%S")
+    df['DATETIME2'] = pd.to_datetime(df.iloc[2:len(df),len(df.columns)-1], format="%d%b%y %H:%M:%S")
+    
     # In[53]:
     # Change datetime column format to look like 01/31/2015 0:00:00
     df['DATETIME2'] = df['DATETIME2'].dt.strftime('%m/%d/%Y %H:%M:%S')
@@ -65,6 +79,21 @@ for i in range(n):
     df1 = df[0:1]  # FIRST ROW
     df2 = df[1:len(df)]  # SECOND TO LAST ROW
 
+    # Convert columns to numbers those that has string wont be converted    
+    df2 = df2.apply(lambda x: pd.to_numeric(x, errors='ignore'))
+    
+    # We need to do a for loop to go thru each column and convert  the characters string to numbers
+    
+    # Here we need to go thru the columns that are objects only
+    cols = df2.filter(like='FNR').select_dtypes(include=['object']).columns
+    
+    # Here we need to go thru the columns that are objects only
+    y = 0    
+    for y in range(len(cols)):
+        # Here we are assign a integer to each unique row value, the integer will increase
+        # from 0 to x where x is the total unique values
+        df2[[df2.columns.get_loc(cols[y])]] = pd.factorize(df2.iloc[:,df2.columns.get_loc(cols[y])])[0]
+    
     # Converting Historian files to VTQ format (DATETIME, TAGNAME, DESCRIPTION, VALUE)
     mdf = pd.merge(pd.melt(df1, id_vars=['DATETIME'], var_name='TAGNAME',
                            value_name='DESCRIPTION')[['TAGNAME', 'DESCRIPTION']],
@@ -78,4 +107,5 @@ for i in range(n):
     print("")
 
     # Exporting PHD Tag CSV file
-    mdf.to_csv(CSVPath1 + '/' + str(CSVFileList[i].replace('.csv', '')) + '_Formatted.csv', index=False)
+    for j in range(len(mdf))/999999: #need to round this
+        mdf[j:999999].to_csv(CSVPath1 + '/' + str(CSVFileList[i].replace('.csv', '')) + '_Formatted.csv', index=False)
