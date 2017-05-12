e776bcc4808f6f3cbbec5e1b79d0fcd43fe89d67
Jimmy Vivas <vivasjimmy@gmail.com>
2017-05-12 09:27:22 -0400

    modofied CSVformattingTool to support date and time together. Working on CSVSplitter

--- CSVFormattingTool.py
+++ CSVFormattingTool.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[10]:
+
 # latest update
 # April 26, 2017
 # Changes done by: Jimmy Vivas
@@ -13,18 +18,27 @@
 #   Using Previse or any other tool
 # The code will split the csv file if the result is more than 1MM rows
 
-# In[1]:
+
+# In[11]:
+
 from os import listdir
 import pandas as pd
 import math
-# In[2]:
+
+
+# In[12]:
+
 CSVFileList = []
 NanPHDTagList = []
+
+
+# In[13]:
+
 # mac Path
 #CSVPath1 =  '/Users/jvivas/Documents/XMT Baytwon Sensor Data' \
 #            '/Sensor Data/Testing'
 
-CSVPath1 = '/Users/jvivas/Documents/Aspen/ProMV Beaumont/To be processed'
+CSVPath1 = '/Users/jvivas/Documents/Aspen/PTT GC PoC/Sensor Data/A210 - Quench Oil Tower/To Be Processed'
 
 # Windows Path
 # CSVPath1 = 'C:/Users/jvivas/Dropbox/Mtell Customer Projects/XOM BayTown RHC MEA Tower Foaming/Sensor Data/' \
@@ -32,7 +46,9 @@ CSVPath1 = '/Users/jvivas/Documents/Aspen/ProMV Beaumont/To be processed'
 
 # CSVPath1 = 'C:/Mtell/Projects/XOM Baytown POC/Sensor Data/ToBeFormatted'
 
-# In[3]:
+
+# In[14]:
+
 CSVFileListAll = listdir(CSVPath1)
 m = len(CSVFileListAll)
 for i in range(m):
@@ -42,8 +58,21 @@ for i in range(m):
     if fileExt == "csv":
         CSVFileList.append(fileNameStr)
 
+
+# In[15]:
+
 n = len(CSVFileList)
-# In[4]:
+
+
+# In[16]:
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+
+
+# In[17]:
+
+get_ipython().magic('debug')
 i = 0
 for i in range(n):
     csvfile = CSVPath1 + "/" + CSVFileList[i]
@@ -51,72 +80,47 @@ for i in range(n):
 
     # In[43]:
     df = pd.read_csv(csvfile, index_col=False, sep=',', low_memory=False)
-
-    # In[100]:
-    # Here we add [UNIT] to the description
-    # for x in range(len(df.columns)-2):
-    #     if (df.iloc[1][x+2] != ' ') and (df.iloc[1][x+2] != '-') and (df.iloc[1][x+2] != 'NONE'):
-    #         df.iloc[0][x+2] = df.iloc[0][x+2] + ' [' + df.iloc[1][x+2] + ']'
-
-    # Here we remove the row #1 which is the UNIT row
-    # df = df.drop(df.index[[1]])
-
-    # In[44]:
-    # Concatenate DATE and TIME Column
-    # df['DATETIME2'] = df['DATE'] + ' ' + df['TIME']
-
-    # Replace the "-" in case datetime is following format: "05-Jan-2015"
-    df['ProcessDate'].replace({'-': ''}, inplace=True, regex=True)
+    
     # In[51]:
+    # Need to do this to avoid 
+    df = df.copy()
     # Change new datetime column to datetime format
-    # df['DATETIME2'] = pd.to_datetime(df['DATETIME2'], format="%d%b%y %H:%M:%S")
-    df['ProcessDate'] = pd.to_datetime(df.iloc[1:len(df),len(df.columns)-1], format="%d%b%y %H:%M:%S")
-
+    df['Date TIme'] = pd.to_datetime(df['Date TIme'])
     # In[53]:
     # Change datetime column format to look like 01/31/2015 0:00:00
-    df['ProcessDate'] = df['ProcessDate'].dt.strftime('%m/%d/%Y %H:%M:%S')
-
-    # In[59]:
-    # Work around to move datetime column to be the first column in the dataset
-    # df['DATE'] = df['DATETIME2']
-
-    # In[63]:
-    # Rename DATE column to DATETIME
-    df = df.rename(columns={'DATE': 'ProcessDate'})
-
-    # In[61]:
-    # Delete unnecessary columns
-    # del df['TIME']
-    # del df['DATETIME2']
+    df['Date TIme'] = df['Date TIme'].dt.strftime('%m/%d/%Y %H:%M:%S')
 
     # Code to do Transposing
     # Create two dataframes df1 only with tags and descriptions. df2 tag with values
     df1 = df[0:1]  # FIRST ROW
     df2 = df[1:len(df)]  # SECOND TO LAST ROW
+    
+    # Replacing known test to null
+    df2.replace({'Bad': 'null', 'No Data': 'null', 'error': 'null'}, inplace=True, regex=True)
 
     # Convert columns to numbers those that has string wont be converted
     df2 = df2.apply(lambda x: pd.to_numeric(x, errors='ignore'))
 
+    # Here we filter the columns that are objects and create a dataframe with those columns
+    cols = df2.filter(like='I4').select_dtypes(include=['object']).columns
+
     # We need to do a for loop to go thru each column and convert  the characters string to numbers
-
-    # Here we need to go thru the columns that are objects only
-    cols = df2.filter(like='FNR').select_dtypes(include=['object']).columns
-
-    # Here we need to go thru the columns that are objects only
     y = 0
     for y in range(len(cols)):
+        # We don't want to change null to number
+        if pd.unique(df2[[cols[1]]].values.ravel()) != 'null':
         # Here we are assign a integer to each unique row value, the integer will increase
         # from 0 to x where x is the total unique values
-        df2[[df2.columns.get_loc(cols[y])]] = pd.factorize(df2.iloc[:,df2.columns.get_loc(cols[y])])[0]
+            df2[[df2.columns.get_loc(cols[y])]] = pd.factorize(df2.iloc[:,df2.columns.get_loc(cols[y])])[0]
 
     # Converting Historian files to VTQ format (DATETIME, TAGNAME, DESCRIPTION, VALUE)
-    mdf = pd.merge(pd.melt(df1, id_vars=['DATETIME'], var_name='TAGNAME',
+    mdf = pd.merge(pd.melt(df1, id_vars=['Date TIme'], var_name='TAGNAME',
                            value_name='DESCRIPTION')[['TAGNAME']],
-                   pd.melt(df2, id_vars=['DATETIME'],  var_name='TAGNAME',
+                   pd.melt(df2, id_vars=['Date TIme'],  var_name='TAGNAME',
                            value_name='VALUE'), on=['TAGNAME'])
 
     # Sort columns by VTQ format
-    mdf = mdf[['DATETIME', 'TAGNAME', 'VALUE']]
+    mdf = mdf[['Date TIme', 'TAGNAME', 'VALUE']]
 
     # Exporting PHD Tag CSV file
     j = 1
@@ -142,3 +146,9 @@ for i in range(n):
                 print('Exporting ' + str(CSVFileList[i].replace('.csv', '')) + ' chunk' + str(j) + ' Historian File')
                 print("")
                 mdf[a:b].to_csv(CSVPath1 + '/' + str(CSVFileList[i].replace('.csv', '')) + '_Formatted_chunk' + str(j) + '.csv', index=False)
+
+
+# In[ ]:
+
+
+
